b7c306c4b9911747e07c3255b713df9a1758e32a
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-12-29 10:42:27 -0500

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './css/containerApp.css';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="containerApp">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
